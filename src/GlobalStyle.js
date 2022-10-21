ca496bb5dad1b6e11b07eeb8b23fabb7c12752a5
Radosław Jeleń <jelu140@gmail.com>
2022-10-21 11:33:44 +0200

    adding font type and some styles to GlobalStyle

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -10,5 +10,9 @@ export const GlobalStyle = createGlobalStyle`
   body {
     word-break: break-word;
     background-color: ${({ theme }) => theme.colors.whiteLilac};
+    padding: 110px 14px;
+    font-family: 'Inter', sans-serif;
+    font-size: 18px;
+    color: ${({ theme }) => theme.colors.slateGray};
   }
 `;
