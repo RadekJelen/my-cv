30b703729b2d34d3074af556e536143f14d188f7
Radosław Jeleń <jelu140@gmail.com>
2022-10-21 11:18:43 +0200

    adding Styled Components and GlobalStyle

---
+++ src/GlobalStyle.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    box-sizing: border-box;
+  }
+  *, ::after, ::before {
+    box-sizing: inherit;
+  }
+  body {
+    word-break: break-word;
+  }
+`;
--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { GlobalStyle } from './GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
 );
