4adbf60a94c0e415ca2d8ffeaa05ce79813329b5
Radosław Jeleń <jelu140@gmail.com>
2022-11-08 14:56:21 +0100

    adding media queries to Footer

--- src/common/Footer/styled.js
+++ src/common/Footer/styled.js
@@ -6,11 +6,16 @@ export const Wrapper = styled.div`
 `;
 
 export const LetsTalk = styled.p`
+  margin-bottom: 24px;
   text-transform: uppercase;
   font-weight: 700;
   font-size: 12px;
   line-height: 130%;
   color: ${({ theme }) => theme.colors.slateGray};
+
+    @media (max-width: 481px) {
+    margin-bottom: 12px;
+  }
 `;
 
 export const Link = styled.a`
@@ -24,22 +29,30 @@ export const Link = styled.a`
 `;
 
 export const Email = styled.p`
-  margin: 24px 0;
   text-decoration: none;
   color: ${({ theme }) => theme.colors.mineShaft};
   font-weight: 900;
   font-size: 32px;
   line-height: 39px;
   letter-spacing: 0.05em;
+
+  @media (max-width: 481px) {
+    font-size: 18px;
+  }
 `;
 
 export const Paragraph = styled.p`
   max-width: 670px;
-  margin-bottom: 56px;
+  margin: 24px 0 56px;
   color: ${({ theme }) => theme.colors.mineShaft};
   font-size: 18px;
   line-height: 140%;
   letter-spacing: 0.05em;
+
+  @media (max-width: 481px) {
+    font-size: 14px;
+    margin: 12px 0 40px;
+  }
 `;
 
 export const Icons = styled.div`
