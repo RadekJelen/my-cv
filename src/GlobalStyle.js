db0f7d0270a7810d50bdc2d411b89215494d60ce
Radosław Jeleń <jelu140@gmail.com>
2022-11-08 16:01:03 +0100

    changing body padding

--- src/GlobalStyle.js
+++ src/GlobalStyle.js
@@ -10,14 +10,10 @@ export const GlobalStyle = createGlobalStyle`
   body {
     word-break: break-word;
     background-color: ${({ theme }) => theme.colors.whiteLilac};
-    padding: 110px 16px;
+    padding: 32px 16px;
     font-family: 'Inter', sans-serif;
     font-size: 18px;
     color: ${({ theme }) => theme.colors.slateGray};
     letter-spacing: 0.05em;
-
-    @media (max-width: ${({ theme }) => theme.breakpoints.medium}) {
-      padding: 32px 16px;
-    }
   }
 `;
