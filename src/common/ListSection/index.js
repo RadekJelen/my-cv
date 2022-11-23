44184ae3995af74e3fc37893ebb60a6aa4eed804
Radosław Jeleń <jelu140@gmail.com>
2022-11-23 17:01:26 +0100

    refactoring code

--- src/common/ListSection/index.js
+++ src/common/ListSection/index.js
@@ -1,9 +1,11 @@
 import { Line, List, Title, Wrapper } from "./styled";
 
+const maxItemsList = 12;
+
 export const ListSection = ({ title, items }) => (
   <Wrapper>
     <Title>{title}</Title>
     <Line />
-    <List longList={items.length > 12}>{items}</List>
+    <List longList={items.length > maxItemsList}>{items}</List>
   </Wrapper>
 );
